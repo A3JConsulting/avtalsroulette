220bb2b8c96985f622b5c9ead5d752ca9b89defd
Joel Bonander <joel.bonander@a3j.se>
2017-03-17 15:42:58 +0100

    Add code for Google Cloud Function, to fetch agreements

---
+++ cloudFunction/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config({silent: true});
+
+const db = require('knex')({
+  client: 'mysql',
+  connection: process.env.DB_URL
+});
+
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.getAgreements = function helloWorld(req, res) {
+  db('agreement')
+    .join('contract', 'agreement.contract_id', 'contract.id')
+    .select('agreement.created_at', 'agreement.name', 'agreement.signature', 'contract.summary')
+    .orderBy('agreement.created_at', 'desc')
+    .then(result => {
+      res.status(200).json(result);
+    }).catch(err => {
+      res.status(500).json({
+        status: 'Internal serverless error'
+      });
+    });
+};
