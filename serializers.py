496fd1eb99ffbc625f25439d265448c1b11261a7
Joel Bonander <joel.bonander@a3j.se>
2017-03-22 10:12:33 +0100

    Remove updated_at field from json output completely

--- serializers.py
+++ serializers.py
@@ -1,6 +1,3 @@
-models.py import json
-
-
 def serialize_contract(c):
     return {'id': c.id, 'title': c.title, 'body': c.body,
             'usage_limit': c.usage_limit, 'agreement_count': c.agreements.count()}
@@ -11,5 +8,4 @@ def serialize_agreement(a):
     return {'id': a.id, 'name': a.name, 'email': a.email,
             'contract': serialize_contract(a.contract),
             'created_at': a.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-            'updated_at': json.dumps(a.updated_at),
             'signature': a.signature}
