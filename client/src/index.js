0a761091c22492f6f8dc972799ba8c27c5399614
Joel Bonander <joel.bonander@a3j.se>
2017-03-31 16:09:48 +0200

    Put index.css with rest of the css files for client

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import reducer from './reducers';
-import './index.css';
+import './styles/index.css';
 
 const store = createStore(
   reducer,
